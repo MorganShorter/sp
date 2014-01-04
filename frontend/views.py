4ce06afa61582a2055cc494180f608bab03c15dc
James Layhe <elicitate@gmail.com>
2014-01-04 14:26:24 +1100

    Basic customer url and view

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,16 @@
+from django.http import Http404
 from django.shortcuts import render
+from frontend.models import Customer
 
 # Create your views here.
+def customer_detail(request, customer_id="1"):
+#    raise Http404
+    try:
+        customer = Customer.objects.get(pk=customer_id)
+        customer_contacts = customer.contacts.all()
+    except Customer.DoesNotExist:
+        raise Http404
+
+#    return render_to_response('base.html', {'customer': customer})
+
+    return render(request, 'base.html', {'customer': customer, 'customer_contacts': customer_contacts})
--- sp/urls.py
+++ sp/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('frontend.views',
+    #url(r'^customer/(?P<id>\d+)/$', 'customer_detail'),
+    url(r'^customer/(?P<customer_id>\d+)/#?$', 'customer_detail'),
+)
+
+urlpatterns += patterns('',
     # Examples:
     # url(r'^$', 'sp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
