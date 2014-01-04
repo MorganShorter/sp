72150a671df75cb584cabf6ebdeb78b5ead13546
Stevo Gibbs <stevo.gibbs@gmail.com>
2014-01-04 12:24:17 +1100

    Test simple direct template view.

--- sp/urls.py
+++ sp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url('','django.views.generic.simple.direct_to_template', {'template': 'base.html', 'mimetype': 'text/html'}),
 )
