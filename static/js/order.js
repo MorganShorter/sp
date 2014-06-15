675e5ff72c97e75eeb4ed6a73885f508afb031e4
Stevo Gibbs <stevo.gibbs@gmail.com>
2014-06-15 20:11:01 +1000

    Revert "taco-tend"
    This reverts commit b2acbec6b0639e22099d3086883a41634f05eca1.

--- static/js/order.js
+++ static/js/order.js
@@ -6,7 +6,7 @@ $(function () {
         collapsible: true
     });
 
-    $("#order_content").dialog({
+    $("#order_content").dialogExtend({
         title: "Show order",
         autoOpen: false,
         width: 800,
@@ -16,32 +16,7 @@ $(function () {
         open: function(){
             $('.add_product_to_order').css('display', 'block');
         }
-    }) 
-    .dialogExtend({
-        "closable" : true,
-        "maximizable" : true,
-        "minimizable" : true,
-        "collapsable" : true,
-        "dblclick" : "collapse",
-        "titlebar" : "transparent",
-        "minimizeLocation" : "right",
-        "icons" : {
-          "close" : "ui-icon-circle-close",
-          "maximize" : "ui-icon-circle-plus",
-          "minimize" : "ui-icon-circle-minus",
-          "collapse" : "ui-icon-triangle-1-s",
-          "restore" : "ui-icon-bullet"
-        },
-        "load" : function(evt, dlg){ alert(evt.type); },
-        "beforeCollapse" : function(evt, dlg){ alert(evt.type); },
-        "beforeMaximize" : function(evt, dlg){ alert(evt.type); },
-        "beforeMinimize" : function(evt, dlg){ alert(evt.type); },
-        "beforeRestore" : function(evt, dlg){ alert(evt.type); },
-        "collapse" : function(evt, dlg){ alert(evt.type); },
-        "maximize" : function(evt, dlg){ alert(evt.type); },
-        "minimize" : function(evt, dlg){ alert(evt.type); },
-        "restore" : function(evt, dlg){ alert(evt.type); }
-      });
+    });
     $("#order_find").dialog({
         title: "Find order",
         autoOpen: false,
